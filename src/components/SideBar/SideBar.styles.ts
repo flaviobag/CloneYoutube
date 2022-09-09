49ed09290302d62db1f6efc74060338eea0945c3
flaviobag <92397080+flaviobag@users.noreply.github.com>
2022-09-09 08:30:42 -0300

    Sidebar

---
+++ src/components/SideBar/SideBar.styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const NavBar = styled.nav`
+  display: flex;
+  background-color: ${({ theme }) => theme.colors.secondary};
+  height: 968px;
+  width: 240px;
+`;
---
+++ src/components/SideBar/SideBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { NavBar } from './SideBar.styles';
+
+function SideBar() {
+  return <NavBar />;
+}
+
+export default SideBar;
